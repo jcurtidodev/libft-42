381bc14bd103d2b5c87a0d47f8133be380059a10
José Antonio Curtido Cortés <157604691+jcurtidodev@users.noreply.github.com>
2025-03-18 18:06:00 +0100

    Update ft_strtod.c
    Correción de errores de header y variables sin usar

--- ft_strtod.c
+++ ft_strtod.c
@@ -6,13 +6,12 @@
 /*   By: jcurtido <jcurtido@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:37:09 by jcurtido          #+#    #+#             */
-/*   Updated: 2025/02/03 20:14:44 by jcurtido         ###   ########.fr       */
+/*   Updated: 2025/03/18 17:47:17 by jcurtido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "fract_ol.h"
-#include "libft/libft.h"
+#include "libft.h"
 
 static int	ft_isspace(char c)
 {
@@ -41,14 +40,10 @@ double	ft_simple_strtod(const char *str)
 	double	result;
 	double	fraction;
 	int		sign;
-	int		divisor;
-	int		len;
 
 	result = 0.0;
 	fraction = 0.0;
 	sign = 1;
-	divisor = 1;
-	len = 0;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
